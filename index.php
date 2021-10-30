81979676788d41d073f2fe1a7a87731bf16083ec
les-breeze <59539730+les-breeze@users.noreply.github.com>
2021-10-30 00:13:21 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" referrerpolicy="no-referrer" />
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <img src="user.png" alt="background" class="logo">
+            
+            <div class="form-container">
+                <h3>LOGIN HERE</h3>
+                <form action="" class="form">
+                    <form action="" class="form">
+                        <input type="text" placeholder="UserID">
+                        <a href="#">Forgot userId</a>
+                        <button>LOGIN &nbsp; <span class="fa fa-paper-plane"></span></button>
+                </form>
+                <p>Don't have an account yet? <a href="registration.php">Create an account here</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <img src="user.png" alt="background" class="logo">
+            
+            <div class="form-container signup">
+                <h3>CREATE AN ACCOUNT</h3>
+                <form action="" class="form">
+                    <form action="" class="form">
+                        <input type="text" placeholder="Username">
+                        <input type="text" placeholder="Email address">
+                        <input type="text" placeholder="Password">
+                        <input type="text" placeholder="Confirm Password">
+                        <button>SIGN UP &nbsp; <span class="fa fa-arrow-right"></span></button>
+                </form>
+                <p>Already have an account? <a class="navigate-btn" href="index.php">Login here</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
